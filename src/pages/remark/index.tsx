3e4cd84189f0cd494345a538f78c6d1da7fe1e1a
Kenny Cornelius <knycornelius@gmail.com>
2024-06-13 21:17:35 +0700

    Test app with remark, next-mdx, and next-mdx-remote

---
+++ src/pages/remark/index.tsx
@@ -0,0 +1,29 @@
+import { getSortedPostsData } from "../../../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <section>
+      <h2>Blog</h2>
+      <ul>
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
